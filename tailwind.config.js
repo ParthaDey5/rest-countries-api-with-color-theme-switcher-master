03f4eac08fa42bd0442ca5cef2f6589fb403b46a
ParthaDey5 <parthadey.hm@gmail.com>
2025-07-10 14:01:59 +0530

    ✨ Update for GitHub Pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,23 @@
-content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    
-  darkMode: 'class', // <— this activates dark: utility support      
+  content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
 
-    theme: {
-      extend: {
-        transitionProperty: {
-          colors: 'background-color, border-color, color, fill, stroke',
-        },
-        opacity: {
-            65: '0.65',
-          },
-            custom: '0px 0px 10px 3px rgba(0, 0, 0, 0.1)',
-            custom2: '0px 0px 1dvw 1px rgba(0, 0, 0, 0.1)',
-          },  
+  darkMode: 'class',
+
+  theme: {
+    extend: {
+      transitionProperty: {
+        colors: 'background-color, border-color, color, fill, stroke',
       },
-      plugins: [],
+      opacity: {
+        65: '0.65',
+      },
+      boxShadow: {
+        custom:  '0px 0px 10px 3px rgba(0, 0, 0, 0.1)',
+        custom2: '0px 0px 1dvw 1px rgba(0, 0, 0, 0.1)',
+      },
     },
-  };
-    
+  },
+
+  plugins: [],
+}
