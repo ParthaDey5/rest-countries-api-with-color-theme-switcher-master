80c2026e0901d8990d2ab97e911d3875f71eef67
Partha Dey <parthadey.hm@gmail.com>
2026-06-12 02:26:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -59,7 +59,7 @@ function App() {
   return (
     <>
       <div
-        className={` max-w-screen min-h-[1024px] overflow-x-hidden transition-all duration-400 ease-linear ${
+        className={`sm:pt-[5dvw] pt-[15dvw] max-w-screen min-h-[1024px] overflow-x-hidden transition-all duration-400 ease-linear ${
           darkMode ? "Blue950" : "Grey50"
         }`}
       >
