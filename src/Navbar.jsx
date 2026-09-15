7c336e244fc605e3af08f1593196f877645c787f
Partha Dey <parthadey.hm@gmail.com>
2026-09-16 01:01:42 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -24,7 +24,7 @@ function Navbar() {
           onClick={toggleDarkMode}
         >
           <i className="fa-regular fa-moon"></i>
-          <h3 className="nunito-sans-700">Dark Mode</h3>
+          <h3 className="nunito-sans-700">{darkMode?"Light Mode":"Dark Mode"}</h3>
         </button>
       </nav>
     </div>
