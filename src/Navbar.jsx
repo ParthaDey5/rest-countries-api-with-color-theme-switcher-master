fe2914421b5cb1ee9754f5d329d47c9dca23cc4f
Partha Dey <parthadey.hm@gmail.com>
2026-09-16 02:09:16 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -15,7 +15,7 @@ function Navbar() {
     <div>
       <nav
         className={`fixed top-0 inset-x-0 !z-50 overflow-x-hidden sm:h-[5dvw] h-[15dvw] sm:px-[5dvw] px-[4dvw] shadow flex items-center justify-between transition-colors duration-400 ease-linear ${
-          darkMode ? "bg-[#2b3945]" : "bg-white"
+          darkMode ? "bg-[#2b3945] text-white" : "bg-white"
         }`}
       >
         <h2 className="nunito-sans-900">Where in the world?</h2>
