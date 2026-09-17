4be19ba5af2fa9fff37be4ba5c4dfab3c8c98685
Partha Dey <parthadey.hm@gmail.com>
2026-09-17 23:51:55 +0530

    Update CountryDetails.jsx

--- src/CountryDetails.jsx
+++ src/CountryDetails.jsx
@@ -153,10 +153,10 @@ function CountryDetails() {
                     return (
                       <Link
                         key={country}
-                        to={`/country/${encodeURIComponent(country)}`}
+                        to={`/country/:name`}
                         className="flex w-full m-auto nunito-sans-600 shadow-fluid rounded-[2px] !p-0"
                       >
-                        <button className="!w-full !h-full my-auto rounded-[2px] cursor-pointer text-center sm:px-[0.2dvw] px-[2dvw] sm:py-[0.1dvw] py-[1dvw] hover:scale-111 hover:bg-red-600 transform hover:duration-700">{displayName}</button>
+                        <button className="!w-full !h-full bg-[var(--btn-bg)] my-auto rounded-[2px] cursor-pointer text-center sm:px-[0.2dvw] px-[2dvw] sm:py-[0.1dvw] py-[1dvw] hover:scale-111 hover:bg-red-600 transform hover:duration-700">{displayName}</button>
                       </Link>
                     );
                   })
